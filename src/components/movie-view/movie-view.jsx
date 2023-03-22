6d0ff5937d7c73713960f6e9d25048ce8006a90a
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-03-22 11:05:28 -0600

    add Row breakpoints for responsiveness

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,37 +1,51 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./movie-view.scss";
+
 export const MovieView = ({ movie, onBackClick }) => {
   const [genreName, genreDescription] = movie.genre;
   const [directorName, directorBio] = movie.director;
   return (
-    <div>
-      <div>
-        <img src={movie.image} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <br></br>
-      <div>
-        <span>Genre </span>
+    <Row className="justify-content-md-center" sm={1} md={2}>
+      <Col>
+        <div>
+          <img src={movie.image} className="w-100" />
+        </div>
+      </Col>
+      <Col>
+        <div>
+          <span>Title: </span>
+          <span>{movie.title}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
         <br></br>
-        <span> Name: {genreName}</span>
+        <div>
+          <span>Genre </span>
+          <br></br>
+          <span> Name: {genreName}</span>
+          <br></br>
+          <span>Description: {genreDescription}</span>
+        </div>
         <br></br>
-        <span>Description: {genreDescription}</span>
-      </div>
-      <br></br>
-      <div>
-        <span>Director </span>
+        <div>
+          <span>Director </span>
+          <br></br>
+          <span>Name: {directorName}</span>
+          <br></br>
+          <span>Bio: {directorBio}</span>
+        </div>
         <br></br>
-        <span>Name: {directorName}</span>
-        <br></br>
-        <span>Bio: {directorBio}</span>
-      </div>
-      <br></br>
-      <button onClick={onBackClick}>Back</button>
-    </div>
+        <button
+          onClick={onBackClick}
+          className="back-button"
+          style={{ cursor: "pointer" }}
+        >
+          Back
+        </button>
+      </Col>
+    </Row>
   );
 };
