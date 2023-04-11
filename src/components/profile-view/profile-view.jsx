1f46d40e2140fd9648ce919e31bf8cde54408036
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-04-11 13:35:27 -0600

    add Favorite movies ux

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "../../index.scss";
+import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = ({ user, token }) => {
+export const ProfileView = ({ user, token, movies }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -26,33 +28,45 @@ export const ProfileView = ({ user, token }) => {
   };
 
   return (
-    <Row>
-      <Card className="bg-primary border border-5 p-4">
+    <div>
+      <Row>
+        <Card className="bg-primary border border-5 p-4">
+          <Card.Title className="fw-bold fs-5 text-decoration-underline mb-4">
+            USER PROFILE
+          </Card.Title>
+          <Card.Body className="bg-primary mb-3">
+            <Card.Text>Username: {user.username}</Card.Text>
+            <Card.Text>Email: {user.email}</Card.Text>
+            <Card.Text>Birthday: {user.birthday}</Card.Text>
+            <Button
+              as={Link}
+              to="/updateprofile"
+              className="bg-secondary"
+              variant="primary"
+            >
+              Edit Profile
+            </Button>
+            <br />
+            <Button
+              className="bg-secondary"
+              variant="primary"
+              onClick={handleSubmit}
+            >
+              Delete Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </Row>
+      <Row className="bg-primary border border-5 p-4">
         <Card.Title className="fw-bold fs-5 text-decoration-underline mb-4">
-          USER PROFILE
+          FAVORITE MOVIES
         </Card.Title>
-        <Card.Body className="bg-primary mb-3">
-          <Card.Text>Username: {user.username}</Card.Text>
-          <Card.Text>Email: {user.email}</Card.Text>
-          <Card.Text>Birthday: {user.birthday}</Card.Text>
-          <Button
-            as={Link}
-            to="/updateprofile"
-            className="bg-secondary"
-            variant="primary"
-          >
-            Edit Profile
-          </Button>
-          <br />
-          <Button
-            className="bg-secondary"
-            variant="primary"
-            onClick={handleSubmit}
-          >
-            Delete Profile
-          </Button>
-        </Card.Body>
-      </Card>
-    </Row>
+        {movies.map((movie) => (
+          <Col className="mb-5" sm={6} md={6} lg={4}>
+            <MovieCard key={movie.id} movie={movie} />
+          </Col>
+        ))}
+      </Row>
+    </div>
   );
 };
