301e7f5d9e850a7910a4f0a763d4a3b58af7f0ab
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-04-13 11:15:21 -0600

    add link to login route

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -31,7 +31,9 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/profile">
                   Profile
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link onClick={onLoggedOut} as={Link} to="/">
+                  Logout
+                </Nav.Link>
               </>
             )}
           </Nav>
