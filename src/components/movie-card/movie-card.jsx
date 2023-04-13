84c5c00e2c6bc303cc6fd2cfbb039b38dbd01970
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-04-13 15:42:31 -0600

    update styling, remove accordion

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,23 +5,10 @@ import "./movie-card.scss";
 
 export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100 bg-secondary border border-3">
-      <Accordion className="h-100 bg-secondary">
-        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Card.Img className="card-img" variant="top" src={movie.image} />
-        </Link>
-        <Accordion.Header className="">
-          <Card.Title>{movie.title}</Card.Title>
-        </Accordion.Header>
-        <Accordion.Body className="p-1">
-          <Card.Body>
-            <Card.Text>{movie.description}</Card.Text>
-            <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-              <Button variant="link">Open</Button>
-            </Link>
-          </Card.Body>
-        </Accordion.Body>
-      </Accordion>
+    <Card className="h-100 bg-primary">
+      <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+        <Card.Img className="card-img" variant="top" src={movie.image} />
+      </Link>
     </Card>
   );
 };
